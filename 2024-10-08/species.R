4a9766cd4bf5317f57725172cdbb69993e6285a0
deblina <deblina@uchicago.edu>
2024-10-10 17:32:56 -0400

    this is kinda meh

--- 2024-10-08/species.R
+++ 2024-10-08/species.R
@@ -27,3 +27,29 @@ labs(x = "",
      y = "", 
      title = "Where were the extinct animals?") + 
   theme(plot.title = element_text(hjust = 0.5))
+
+
+# pairwise correlations between references and observations 
+df %>% 
+  select(References, Observations, ParkName) %>% 
+  group_by(ParkName) %>% 
+  summarize(cor = cor(References, Observations)) %>% 
+  mutate(cor_sign = ifelse(cor >= 0, "Positive", "Negative")) %>% 
+  drop_na() %>% 
+  ggplot() + 
+  geom_col(aes(ParkName, cor, fill = cor_sign)) + 
+  scale_fill_manual(values = c("Positive" = "lightgreen", "Negative" = "red")) + 
+  theme_minimal() + 
+  ylim(-1, 1) + 
+  coord_flip() + 
+  labs(x = "",
+       y = "", 
+       title = "Correlation between observations and references per Park") + 
+  theme(
+    plot.title = element_text(hjust = 0),
+    plot.title.position = "plot",
+    legend.title =  element_blank()
+  )
+
+
+
